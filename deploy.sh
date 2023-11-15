7b70c39768769f610351d73e195012a6e3e5696b
wk <1424257169@qq.com>
2023-11-15 17:37:45 +0800

    7

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ set -e
 
 push_addr="git@github.com:SucessK/SucessK.github.io.git" # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
-dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
+dist_path="docs/.vuepress/dist" # 打包生成的文件夹路径
+push_branch="gh-pages" # 推送的分支
 
 # 生成静态文件
 npm run build
